741ae2df6dcdb5652669c55d83a746e403e8aa21
Saksham Negi <saksham@Sakshams-MacBook-Air.local>
2024-01-10 15:08:33 +0530

    cors error

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 dotenv.config();
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
@@ -19,6 +20,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(cors());
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
